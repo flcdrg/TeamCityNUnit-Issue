b222b814d007e11d09623466fb52496578d0d75a
David Gardiner <david@gardiner.net.au>
2016-06-06 18:41:11 +0930

    initial

--- TeamCityNUnit/UnitTest1.cs
+++ TeamCityNUnit/UnitTest1.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace TeamCityNUnit
 {
-    [TestClass]
+    [TestFixture]
+    [Category("Special")]
     public class UnitTest1
     {
-        [TestMethod]
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            Console.WriteLine("Inside TestFixtureSetUp");    
+        }
+
+        [Test]
         public void TestMethod1()
         {
+        }
+
+        [Test]
+        [Explicit("This is explicit")]
+        public void ShouldBeExplicit()
+        {
+            
         }
     }
 }
